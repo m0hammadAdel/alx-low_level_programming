02de94d975bb9dd3c8d4d421ba22d2b22025a102
Mohammad Adel <mohammadadel8727@gmail.com>
2023-09-28 11:32:04 -0700

    added 5 mandatory tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	while(*s != '\0'){
+	if (*s)
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+
+	else
+		_putchar('\n');
 }
