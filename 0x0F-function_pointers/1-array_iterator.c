89d94f1cd0567558ccc5bec356a7f0497572b6ed
Mohammad Adel <mohammadadel8727@gmail.com>
2023-10-13 00:21:15 -0700

    fixed the header call

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,21 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * array_iterator - apply the function argument to all array elements
- * @array: array arugment
- * @size: size of the array
- * @action: function pointer to perform
- * Return: nothing, void function
- **/
-
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
-		return 0;
+		return;
 
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
